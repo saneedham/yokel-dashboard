91f0b48bd33225ce8cbf128e1f25cc4d0e4c84d2
Simon Needham <saneedham@gmail.com>
2020-03-18 22:46:11 +0000

    Fixed mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'mdbootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
