2f0dc046d8c1583220049ea384495ddfa845c7a1
Simon Needham <saneedham@gmail.com>
2020-03-23 22:25:00 +0000

    Removed Inventory-Item as it caused display errors. Added tests for items within the table.

--- src/app/inventory-table/inventory-table.component.spec.ts
+++ src/app/inventory-table/inventory-table.component.spec.ts
@@ -17,6 +17,7 @@ describe('InventoryTableComponent', () => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    const el = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
@@ -26,36 +27,186 @@ describe('InventoryTableComponent', () => {
   it(`should have a column with the heading 'Product name'`, async(() => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('th[name="col1"]').textContent).toEqual('Product name');
+    const el = fixture.debugElement.nativeElement;
+    expect(el.querySelector('th[name="col1"]').textContent).toEqual('Product name');
   }));
 
   it(`should have a column with the heading 'Unit cost (£)'`, async(() => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('th[name="col2"]').textContent).toEqual('Unit cost (£)');
+    const el = fixture.debugElement.nativeElement;
+    expect(el.querySelector('th[name="col2"]').textContent).toEqual('Unit cost (£)');
   }));
 
   it(`should have a column with the heading 'Stock level'`, async(() => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('th[name="col3"]').textContent).toEqual('Stock level');
+    const el = fixture.debugElement.nativeElement;
+    expect(el.querySelector('th[name="col3"]').textContent).toEqual('Stock level');
   }));
 
   it(`should have a column with the heading 'Stock value'`, async(() => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('th[name="col4"]').textContent).toEqual('Stock value');
+    const el = fixture.debugElement.nativeElement;
+    expect(el.querySelector('th[name="col4"]').textContent).toEqual('Stock value');
   }));
 
   it(`should have a column with the heading 'Actions'`, async(() => {
     fixture = TestBed.createComponent(InventoryTableComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('th[name="col5"]').textContent).toEqual('Actions');
+    const el = fixture.debugElement.nativeElement;
+    expect(el.querySelector('th[name="col5"]').textContent).toEqual('Actions');
   }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have a column with the value 'Black Pepper Sausages'`, async(() => {
+    expect(el.querySelector('td[name="product"]').textContent).toEqual('Black Pepper Sausages');
+  }));
+
+  it(`should have a column with the value '3.50'`, async(() => {
+    expect(el.querySelector('td[name="cost"]').textContent).toEqual('3.50');
+  }));
+
+  it(`should have a column with the value '3'`, async(() => {
+    expect(el.querySelector('td[name="stock"]').textContent).toEqual('3');
+  }));
+
+  it(`should have a column with the value '10.50'`, async (() => {
+    expect(el.querySelector('td[name="value"]').textContent).toEqual('10.50');
+  }));
+
+  it(`should have a table row with the id 'a2b3e4f5g2h1`, async(() => {
+    expect(el.querySelector('tr').id).toEqual('a2b3e4f5g2h1');
+  }));
+
+  it(`should have a button with the prompt 'Edit stock level'`, async (() => {
+    expect(el.querySelectory('button[id="editsave"]').textContent).toEqual('Edit stock level');
+  }));
+
+  it(`should call editStockClick() and make stock column editable`, async(() => {
+    fixture.editStockClick();
+    expect(el.querySelector(`td[name="stock"]`).contentEditable).toBeTruthy();
+  }));
+
+  it(`should call editStockClick() and change the text of btn.editSave to 'Save'`, async(() => {
+    fixture.editStockClick();
+    expect(el.querySelector('button[id="editSave"]').textContent).toEqual('Save');
+  }));
+
+  it(`should call editStockClick() and change the class of the btn.editSave to btn-success`, async(() => {
+    fixture.editStockClick();
+    expect(el.querySeletor('button[id="editSave"]').class).toContain('btn-success');
+  }));
+
+  it (`should call editStockClick() and change the onclick call of btn.editSave to saveStockClick()`, async(() => {
+    fixture.editStockClick();
+    expect(el.querySelector('button[id="editSave"]').onclick).toEqual('saveStockClick');
+  }));
+
+  it(`should change the class of btn.editSave from btn-primary to btn-warning when the value of stock changes`, async(() => {
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = 4;
+    expect(el.querySelector('button[id="editSave"]').class).toContain('btn-warning');
+  }));
+
+  it(`should call editStockClick() and make btn.cancel visible`, async(() => {
+    fixture.editStockClick();
+    expect(el.querySelector('button[id="cancel"]').hidden).toBeFalsy();
+  }));
+
+  it(`should call saveStockClick() and change the class of btn.editSave to btn-primary`, async(() => {
+    fixture.editStockClick()
+    el.querySelector('td[name="stock"]').textContent = 4;
+    fixture.saveStockClick()
+    expect(el.querySelector('button[id="editSave"]').class).toContain('btn-primary');
+  }));
+
+  it(`should call saveStockClick() and change the text of btn.editSave to 'Edit'`, async(() => {
+    fixture.editStockClick();
+    fixture.savestockClick();
+    expect(el.querySelector('button[id="editSave"]').textContent).toEqual('Edit');
+  }));
+
+  it(`should call saveStockClick() and hide btn.cancel`, async(() => {
+    fixture.editStockClick();
+    fixture.saveStockClick();
+    expect(el.querySelector('button[id="cancel"]').hidden).toBeTruthy();
+  }));
+
+  it(`should call saveStockClick() and make td.stock uneditable`, async(() => {
+    fixture.editStockClick();
+    fixture.saveStockClick();
+    expect(el.querySelector('td[name="stock"]').contentEditable).toBeFalsy();
+  }));
+
+  it(`should call cancelChanges() and display a modal warning dialog`, async(() => {
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = 4;
+    fixture.cancelChanges();
+    expect(el.querySelector['div[id="warningModal"]']).toHaveClass('Show');
+  }));
+
+  it(`should call goBack() and close the modal warning dialog`, async(() => {
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = 4;
+    fixture.cancelChanges();
+    fixture.goBack();
+    expect(el.querySelector('div[id="warningModal"]')).not.toHaveClass('Show');
+  }));
+
+  it(`should call goBack() and keep the value of stock the same`, async(() => {
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = 4;
+    fixture.cancelChanges();
+    fixture.goBack();
+    expect(el.querySelector('td[name="stock"]').textContent).toEqual('4');
+  }));
+
+  it(`should call cancelConfirm() and close the modal warning dialog`, async(() => {
+    fixture.editStockClick();
+    const originalValue = el.querySelector('td[name="stock"]').textContent;
+    el.querySelector('td[name="stock"]').TextContent = 4;
+    fixture.cancelChanges();
+    fixture.cancelConfirm();
+    expect(el.querySelector('td[name="stock"]').textContent).toEqual(originalValue);
+  }));
+
+  it(`should call cancelConfirm() and make td.stock uneditable`, async(() => {
+    fixture.editStockClick();
+    fixture.cancelChanges();
+    fixture.cancelConfirm();
+    expect(el.querySelector('td[name="stock"]').contentEditable).toBeFalsy();
+  }));
+
+  it(`should call saveStockClick() and update the stock level in the database for this product`, async(() => {
+    /*
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = '4';
+    fixture.saveStockClick(el.querySelector('tr').id);
+
+    Test for success response from db server
+    */
+  }));
+
+  it(`should call saveStockClick() and display an error if an error is returned by the db server`, async(() => {
+/*
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = '4';
+    fixture.saveStockClick(el.querySelector('tr').id);
+
+    Test for error response being displayed to user from db server
+    */
+  }));
+
+  it(`should call saveStockClick() and update the td.value`, async(() => {
+    fixture.editStockClick();
+    el.querySelector('td[name="stock"]').textContent = 4;
+    fixture.saveStockClick();
+    expect(el.querySelector('td[name="value"]').textContent).toEqual(el.querySelector('td[name="stock"]').textContent * el.querySelector('td[name="cost"]').textContent)
+  }))
 
 });
--- src/app/inventory-table/inventory-table.component.ts
+++ src/app/inventory-table/inventory-table.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-inventory-table',
   templateUrl: './inventory-table.component.html',
   styleUrls: ['./inventory-table.component.scss']
 })
 export class InventoryTableComponent implements OnInit {
+
+  product = {
+    name: 'Black Pepper Sausages',
+    cost: '3.50',
+    stock: '3',
+    value: '10.50',
+    id: 'a2b3e4f5g2h1'
+  };
 
   headings: Array<string>= [
     'Product name',
